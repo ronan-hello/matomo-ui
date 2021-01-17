12af9ba924243bab0aa8f3f1b1ba2627c865a35e
Ronan <ronan56.hello@gmail.com>
2021-01-17 21:31:38 +0100

    update: Add main.scss file

---
+++ components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+
+    <NuxtLink to="/" class="navbar-brand">
+      <Logo/>
+    </NuxtLink>
+
+  </header>
+</template>
+
+<script>
+import Logo from './Logo';
+
+export default {
+
+  components: {Logo}
+
+}
+</script>
